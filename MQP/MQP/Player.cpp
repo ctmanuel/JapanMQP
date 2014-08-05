c1ce5e325a0b1ad0ff122582b350bca72bce52ae
Adam Degenhardt <adegenhardt@comcast.net>
2014-08-05 17:02:15 +0900

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	MQP/MQP.sdf
    	MQP/MQP.v12.suo

--- MQP/MQP/Player.cpp
+++ MQP/MQP/Player.cpp
@@ -1,60 +1,159 @@
 #include "Player.h"
-#include "Game.h"
 
 using namespace C4;
 
-PlayerController::PlayerController() : Controller(kControllerPlayer)
+
+MainPlayerController::MainPlayerController(float azimuth) :
+CharacterController(kControllerPlayer),
+playerInteractor(this)
 {
-	PlayerNode = GetTargetNode();
-	SetPlayerModel(Model::Get(kModelPlayer));
+	playerMotion = kMotionNone;
+	movementFlags = 0;
+	
+	speed = START_SPEED;
+
+	modelAzimuth = azimuth;
+	modelAltitude = 0.0F;
+}
+ MainPlayerController::MainPlayerController() :
+CharacterController(kControllerPlayer),
+playerInteractor(this)
+{
+	 speed = START_SPEED;
 }
 
-PlayerController::PlayerController(const PlayerController& playerController) : Controller(playerController)
+ MainPlayerController::MainPlayerController(const MainPlayerController& playerController) :
+CharacterController(playerController),
+playerInteractor(this)
 {
+	playerMotion = kMotionNone;
+	movementFlags = 0;
+	
+	speed = START_SPEED;
 
+	modelAzimuth = 0.0F;
+	modelAltitude = 0.0F;
 }
 
-Controller *PlayerController::Replicate(void) const
+ MainPlayerController::~MainPlayerController()
 {
-	return (new PlayerController(*this));
+	
 }
 
-PlayerController::~PlayerController()
-{
-
+//make duplicate of controller with pointer to current controller
+Controller *MainPlayerController::Replicate(void) const
+ {
+	return (new MainPlayerController(*this));
 }
 
-bool PlayerController::ValidNode(const Node *node)
+ bool MainPlayerController::ValidNode(const Node *node)
 {
-	return ((node) && 
-		((node->GetNodeType() == kNodeModel) ||
-		 (node->GetNodeType() == kNodeGeometry)));
+	return ((node) && (node->GetNodeType() == kNodeModel) || node->GetNodeType() == kNodeGeometry);
 }
 
-void PlayerController::Pack(Packer& data, unsigned long packFlags) const
+ void MainPlayerController::Pack(Packer& data, unsigned long packFlags) const
 {
 	Controller::Pack(data, packFlags);
 }
 
-void PlayerController::Unpack(Unpacker& data, unsigned long unpackFlags)
+ void MainPlayerController::Unpack(Unpacker& data, unsigned long unpackFlags)
 {
 	Controller::Unpack(data, unpackFlags);
 }
 
-void PlayerController::Preprocess(void)
+ void MainPlayerController::Preprocess(void)
 {
-	Controller::Preprocess();
+		//This function is called once before the target node is ever
+			//rendered or moved. The base class PreProcess() function should
+			// always be called first, and then the subclass can do whatever 
+			//preprocessing it needs to do.
+		
+		Controller::Preprocess();
 }
 
-void PlayerController::Move(void)
+ void MainPlayerController::LightpathNode(Node *node){
+	 lightPathNodes.push(node);
+ }
+
+ void MainPlayerController::Move(void)
 {
-	Point3D currentpos = PlayerNode->GetNodePosition();
-	PlayerNode->SetNodePosition(Point3D(currentpos.x += 5.0F, currentpos.y, currentpos.z));
+	 float x = 0;
+	 float y = 0;
+	 float z = 0;
+	 float distance = 1;
+	//This is called once per frame to allow the controller to 
+	//move its target node.
+	 if (lightPathNodes.empty()){
+		 //go straight
+		 x += 0.5;
+		 y += 0.5;
+		 z += 0.5;
+	 }
+	 else{
+		 
+		 Point3D lpos = lightPathNodes.front()->GetNodePosition();
+		 Point3D ppos = GetTargetNode()->GetNodePosition();
+		 distance = std::sqrt(pow(lpos.x - ppos.x, 2) + pow(lpos.y - ppos.y, 2) + pow(lpos.z - ppos.z, 2));
+		 while (distance < 0.2f){
+			 //POP
+			 lightPathNodes.pop();
+			 //check if empty
+			 if (lightPathNodes.empty()){
+				 break;
+			 }
+			 else {
+				 lpos = lightPathNodes.front()->GetNodePosition();
+				 distance = std::sqrt(pow(lpos.x - ppos.x, 2) + pow(lpos.y - ppos.y, 2) + pow(lpos.z - ppos.z, 2));
+
+				 // Face forwards
+				 float rotx = 0;
+				 float roty = 0;
+				 float rotz = 0;
+				 lightPathNodes.front()->GetNodeTransform().GetEulerAngles(&rotx, &roty, &rotz);
+				 Matrix3D rotation;
+				 rotation.SetEulerAngles(rotx, roty, rotz);
+				 GetTargetNode()->SetNodeMatrix3D(rotation);
+			 }
+		 }
+		 float vt = (speed * TheTimeMgr->GetFloatDeltaTime()) / 1000.0F;
+		 x = ((lpos.x - ppos.x) / distance) * vt + ppos.x;
+		 y = ((lpos.y - ppos.y) / distance) * vt + ppos.y;
+		 z = ((lpos.z - ppos.z) / distance) * vt + ppos.z;
+	 }
+
+	 /*GetTargetNode()->SetNodePosition(Point3D(GetTargetNode()->GetNodePosition().x + speed * (TheTimeMgr->GetFloatDeltaTime()/1000.0F)
+		 ,0
+		 ,0));*/
+	 GetTargetNode()->SetNodePosition(Point3D(x, y, z));
 }
 
-Point3D PlayerController::PlayerPosition()
-{
+void MainPlayerController::SetPlayerMotion(int32 motion){
+		//This function sets the animation resource corresponding to 
+			//the current type of motion assigned to the player
+		
+		Interpolator *interpolator = frameAnimator.GetFrameInterpolator();
 	
-	 return PlayerNode->GetNodePosition();
+		if (motion == kMotionStand)
+		 {
+		frameAnimator.SetAnimation("player/Stand");
+		interpolator->SetMode(kInterpolatorForward | kInterpolatorLoop);
+		}
+	else if (motion == kMotionForward)
+		 {
+		frameAnimator.SetAnimation("player/Forward");
+		interpolator->SetMode(kInterpolatorForward | kInterpolatorLoop);
+		}
+	
 }
 
+Point3D MainPlayerController::GetDestination()
+{
+	if (lightPathNodes.empty())
+	{
+		return Point3D(0.0f, 0.0f, 0.0f);
+	}
+	else
+	{
+		return lightPathNodes.front()->GetNodePosition();
+	}
+}
--- MQP/MQP/Player.h
+++ MQP/MQP/Player.h
@@ -1,58 +1,162 @@
 
-#ifndef PLAYER_H
-#define PLAYER_H
+#ifndef _PLAYER_H_
+ #define _PLAYER_H_
 
 #include "C4Controller.h"
+#include "C4Character.h"
 #include "C4World.h"
+#include "C4Engine.h"
 #include "Leap.h"
+#include "PlayerInteractor.h"
+#include <queue>
+
+#define START_SPEED (1.0f) // m/s
+
 
 namespace C4
 {
 	enum
 	{
-		kControllerPlayer = 'Play'
+		 kControllerPlayer = 'plr'
 	};
 	enum
 	{
-		kModelPlayer = 'Play'
+		 kModelPlayer = 'plr'
 	};
 
-	class PlayerController : public Controller
+	class MainPlayerController;
+	
+	class MainPlayer : public Player
 	{
 	private:
-		PlayerController(const PlayerController& playerController);				//private constructor
+			
+		ControllerReg<MainPlayerController>		playerControllerRegistration;
+		ModelRegistration					playerModelRegistration;
+			
+		MainPlayer();
+		~MainPlayer();
+			
+	public:
+					
+		static void Construct(void);
+		static void Destruct(void);
+	};
+	
+		
+	//Controllers are used to control anything that moves in the world.
+	//new types of controllers defined by the application/game module can be
+	//registered with the engine when the Game class is constructed.
+	//
+	//This controlleris used to animate the player. It uses the built-in 
+	//character controller as a base class so that the engine's native 
+	//physics can be used to move the character
+	class MainPlayerController : public CharacterController
+	{
+	private:
+		//motion states that are used to keep track 
+		//of which animation should be played
+			
+		enum
+		{
+		kMotionNone,
+		kMotionStand,
+		kMotionForward,
+		};
+		
+		//movement flags tell how the user is trying to move the  player
+		unsigned_int32		movementFlags;
 
-		Controller *Replicate(void) const;
+		//player motion keeps track of what animation is currently playing
+			
+		int32				playerMotion;
+		
+		//The azimuth and altitude represent the direction the player is looking 
+		//by using the rift
+			
+		float				modelAzimuth;
+		float				modelAltitude;
+		
+		//frame animator controls playback of an animation resource
+			
+		FrameAnimator		frameAnimator;
+		
+		//Previous center of mass stores the center point of the character on the
+		//previous frame. This is used with the new center point to activate triggers
+			
+		Point3D				previousCenterOfMass;
+		
+		//we keep an interactor object here in the controller
+		PlayerInteractor	playerInteractor;
 
-		Node *PlayerNode;														//Player's node
-		Model *PlayerModel;														//Player model
+		//queue of pointers to light path nodes
+		std::queue<Node*>	lightPathNodes;
+
+		//player speed
+		float speed = START_SPEED;
+		
+		MainPlayerController(const MainPlayerController& playerController);				//private constructor
+		
+		Controller *Replicate(void) const override;
+		
+		void SetPlayerMotion(int32 motion);
 
 	public:
 
 		//public constructor
-		PlayerController();
+		MainPlayerController(float azimuth);
+		
+		MainPlayerController();
+		
 		//destructor
-		~PlayerController();
+		~MainPlayerController();
 
+		Model *GetTargetNode(void) const
+		{
+			return (static_cast<Model *>(Controller::GetTargetNode()));
+		}
+
+		unsigned_int32 GetMovementFlags(void) const
+		{
+			return (movementFlags);
+		}
+
+		void SetMovementFlags(unsigned_int32 flags)
+		{
+			movementFlags = flags;
+		}
+
+		float GetModelAzimuth(void) const
+		{
+			return (modelAzimuth);
+		}
+		
+		float GetModelAltitude(void) const
+		{
+			return (modelAltitude);
+		}
+
+		PlayerInteractor *GetPlayerInteractor(void)
+		{
+			return (&playerInteractor);
+		}
+
+		float GetSpeed()
+		{
+			return speed;
+		}
+		
 		static bool ValidNode(const Node *node);
 
+		void LightpathNode(Node *node);
+		void LightpathSpeed(float speed);
+
+		Point3D GetDestination();
+		
 		void Pack(Packer& data, unsigned long packFlags) const;
 		void Unpack(Unpacker& data, unsigned long unpackFlags);
-
+		
 		void Preprocess(void);
 		void Move(void);
-
-		Point3D PlayerPosition();
-		
-		void SetPlayerModel(Model* model)
-		{
-			PlayerModel = model;
-		}
-
-		Model *GetPlayerModel()
-		{
-			return PlayerModel;
-		}
 	};
 }
 
